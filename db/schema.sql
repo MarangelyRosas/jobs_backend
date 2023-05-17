1fdb5d9279bbbef5c36d4363808fdb3b5516bec1
Marangely Rosas <Ladykaliber22@gmail.com>
2023-05-17 16:09:43 -0400

    completed db and all files included

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS jobs_dev;
+CREATE DATABASE jobs_dev;
+
+\c jobs_dev;
+
+CREATE TABLE jobs (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    description TEXT NOT NULL,
+    company VARCHAR(255),
+    location VARCHAR(100),
+    salary NUMERIC CHECK (salary >= 0) -- Adding the CHECK constraint
+    url  TEXT NOT NULL,
+    is_favorite BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE reviews (
+ id SERIAL PRIMARY KEY,
+ reviewer TEXT NOT NULL,
+ title TEXT NOT NULL,
+ content TEXT NOT NULL,
+ rating NUMERIC,
+ CHECK (rating >= 0 AND rating <= 5),
+ job_id INTEGER NOT NULL REFERENCES jobs (id)
+  ON DELETE CASCADE
+);
